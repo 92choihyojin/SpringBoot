6041d2de1da283fd858a9d90d1144a375330c205
choi hyo jin <dkdlelrkdlrj@naver.com>
2025-06-27 15:50:02 +0900

    스프링 부트 " 오라클 의존성 " 추가 /  " domain " 패키지 추가

--- src/main/java/com/kh/ChjImageShopApplication.java
+++ src/main/java/com/kh/ChjImageShopApplication.java
@@ -1,11 +1,13 @@
 package com.kh;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Mapper 인터페이스 스캔 활성화
 @SpringBootApplication
+@MapperScan(basePackages = "com.project.mapper")
 public class ChjImageShopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ChjImageShopApplication.class, args);
 	}
